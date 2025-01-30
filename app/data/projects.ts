a9c9b075d4bc7e13271897cea6d8eaec3e04b6c1
Wen Bin <wenbin_ng@yahoo.com.sg>
2025-01-30 19:31:55 +0800

    task: update jpg to webp to lower asset size

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -5,32 +5,32 @@ export const projects: Project[] = [
     title: "Recyclables",
     description:
       "A Telegram bot connecting residents with rag-and-bone collectors, helping 780 users recycle 4.2 tons of waste effortlessly.",
-    image: "./assets/recyclables.jpg",
+    image: "./assets/recyclables.webp",
     url: "https://recyclables.netlify.app/",
   },
   {
     title: "Impact Giving Map",
     description: "3D Impact data visualisation using Deck.gl",
-    image: "./assets/impact-giving-map.jpg",
+    image: "./assets/impact-giving-map.webp",
   },
   {
     title: "Table with GraphQL server pagination",
     description:
       "Prototype to test headless react table with GraphQL based server pagination",
-    image: "./assets/amazon-clone.jpg",
+    image: "./assets/amazon-clone.webp",
     url: "https://github.com/davidng10/amazon-storefront-clone",
   },
   {
     title: "Musical Sort",
     description: "Algorithmic music sorting display",
-    image: "./assets/musical-sort.jpg",
+    image: "./assets/musical-sort.webp",
     url: "https://github.com/davidng10/Musical-sort",
   },
   {
     title: "Local music player with Electron",
     description:
       "Utilizes a doubly linked list with a shuffle engine, supporting up to 100,000 songs",
-    image: "./assets/react-candlelight.jpg",
+    image: "./assets/react-candlelight.webp",
     url: "https://github.com/davidng10/react-candlelight",
   },
 ];
